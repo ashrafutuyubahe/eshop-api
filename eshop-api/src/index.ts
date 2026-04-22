9285b82b3a91810bc2583f2210c9386c3fa9700a
ashrafutuyubahe <ashraftuyubahe001@gmail.com>
2026-04-22 12:05:05 +0200

    [++] routing, folder structure setup

--- eshop-api/src/index.ts
+++ eshop-api/src/index.ts
@@ -1,12 +1,20 @@
-import { Hono } from 'hono'
-
-const app = new Hono()
-
-app.get('/', (c) => {
-  
-  return c.text('Hello Hono!')
+import {Hono } from "hono"
 
 
+const app= new Hono();
+
+app.get("/",(c)=>{
+   return c.text("Hello from Hono")
 })
+
+// app.on(["DELETE","PATCH"],"/users",(c)=>{  // multiple http requests
+
+//   return  c.json({"users":["kamali","jack"]})
+
+// })
+app.get("/health",(c)=>{
+  return c.text("server is running fine!!!")
+})
+
 
 export default app
