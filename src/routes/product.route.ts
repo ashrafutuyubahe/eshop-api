4dd7bb0e6e32b67d037fd34f91be1562a489c275
ashrafutuyubahe <ashraftuyubahe001@gmail.com>
2026-04-22 14:10:20 +0200

    [++] done  in-memory crud operations

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,42 +1,84 @@
-import {Hono} from "hono"
+import { Hono } from "hono";
 
-const route= new Hono();
+const route = new Hono();
 
-const product:Record<string, string>={
-    1:"product 1",
-    2:"product 2",
-    3:"product 3"
-}
+type product = {
+  id: string;
+  name: string;
+};
 
+const product: product[] = [
+  { id: "1", name: "product 1" },
+  { id: "2", name: "product 2" },
+];
 
+route.get("/", (c) => {
+  return c.json({
+    message: "products retrieved successfully",
+    data: { product },
+  });
+});
 
+route.get("/:id", (c) => {
+  const prodId: string = c.req.param("id");
+  return c.json({
+    message: "products retrieved successfully",
+    data: { product_id: product.find((p) => p.id === prodId) },
+  });
+});
+route.post("/", (c) => {
+  const product_name = c.req.query("name");
 
+  const new_product: product = {
+    id: Date.now().toString(),
+    name: String(product_name),
+  };
+  product.push(new_product);
+  return c.json({
+    message: "product saved successfully",
+    data: new_product,
+  });
+});
 
+route.delete("/:id",(c)=>{
+    const product_id= c.req.param("id")
 
-route.get("/",(c)=>{
+const product_exists= product.find((id)=>id.id===product_id);
 
-    return c.json({
-      message:"products retrieved successfully",
-      data:{product}
-    })
+     if(!product_exists){
+        return c.text(" product with provided Id doesn't exists",404);
+     }
+
+    product.splice(product.findIndex(p=>p.id===c.req.param("id")),1)
+
+    return c.text("product deleted successfully")
 })
 
-route.get("/:id",(c)=>{
-    const prodId:string= c.req.param("id");
-    return c.json({
-      message:"products retrieved successfully",
-      data:{product_id:product[prodId]}
-    })
-})
-route.post("/",(c)=>{
-    const product_name= c.req.query("name");
+route.patch("/:id",async (c)=>{
+    const product_id= c.req.param("id")
+
+    const body:any=  await c.req.json();
     
 
-    product.push(product_name)
-    return c.json({
-        message:"product saved successfully",
-        data:product
-    })
-})
+     const product_exists= product.find((id)=>id.id===product_id);
 
-export default route
+     if(!product_exists){
+        return c.text(" product with provided Id doesn't exists",404);
+     }
+  
+
+      if(!body){
+        return c.text("product name is required",400);
+        
+      }
+
+      product_exists.name=body.product_name;
+      
+
+    return  c.json({
+        message:"product updated successfully",
+        data:product_exists
+    })
+
+})
+export default route;
