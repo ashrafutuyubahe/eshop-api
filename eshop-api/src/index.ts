4b2109dedd62020bf97cb8aa69e7a049138208fc
ashrafutuyubahe <ashraftuyubahe001@gmail.com>
2026-04-22 12:09:10 +0200

    [++] learning hono context

--- eshop-api/src/index.ts
+++ eshop-api/src/index.ts
@@ -12,6 +12,14 @@ app.get("/",(c)=>{
 //   return  c.json({"users":["kamali","jack"]})
 
 // })
+
+app.get("/users/:id",(con)=>{
+  const user_id= con.req.param("id");
+  //  return con.res.json({"user_id":user_id});
+
+  return con.json({"user_id":user_id})
+
+})
 app.get("/health",(c)=>{
   return c.text("server is running fine!!!")
 })
